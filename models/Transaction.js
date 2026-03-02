ddc995eefee3cea0155f2d38b829389945a7bd3d
walidnoussir <noussirw@gmail.com>
2026-03-02 14:29:23 +0000

    model added to the project

---
+++ models/Transaction.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const transactionSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["income", "expense"],
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const Transaction = mongoose.model("Transaction", transactionSchema);
+
+export default Transaction;
