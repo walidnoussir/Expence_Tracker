65620e28550fa9ed5b696c0270a8a6739941df9d
ayoubhanine <ayoubhanine2000@gmail.com>
2026-03-03 11:40:30 +0000

    creation de serveur

---
+++ server.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+const MONGODB_URL = process.env.MONGO_URI;
+
+try {
+  await mongoose.connect(MONGODB_URL);
+  console.log("connected to MongoDB");
+} catch (err) {
+  console.log(err);
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
