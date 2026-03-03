04e1450ce7c35d333d84624c2f4302133a3307ab
walidnoussir <noussirw@gmail.com>
2026-03-03 09:45:51 +0000

    server done

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-import dotenv from 'dotenv';
-import  express from 'express'
-import mongoose from 'mongoose'
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
 
-dotenv.config()
+dotenv.config();
 
-const app=express()
-app.use(express.json())
-const urlMongoDB=process.env.MONGO_URI
-mongoose.connect(urlMongoDB)
-.then(()=>console.log('MongoDB connected'))
-.catch(err=>console.log(err))
+const app = express();
+app.use(express.json());
+const MONGODB_URL = process.env.MONGO_URI;
 
-const PORT=process.env.PORT || 5000;
-app.listen(PORT,
-    ()=>console.log(`Server running on ${PORT}`)
-)
+try {
+  await mongoose.connect(MONGODB_URL);
+  console.log("connected to MongoDB");
+} catch (err) {
+  console.log(err);
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
