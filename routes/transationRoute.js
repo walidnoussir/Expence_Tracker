f82eb3254190f00143e2c2e149c9909bac8cc276
salima <sahisalima77@gmail.com>
2026-03-04 11:24:06 +0100

    ajouter route  les opération statistique

--- routes/transationRoute.js
+++ routes/transationRoute.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { getTransactionById } from '../controllers/GetByIdTransaction.js'
 import { deleteTransaction } from '../controllers/DeleteTransation.js';
+import { getMonthStats } from '../controllers/StatistiqueController.js';
  const router=express.Router()
 
 
@@ -8,6 +9,6 @@ import { deleteTransaction } from '../controllers/DeleteTransation.js';
 
 router.get("/:id",getTransactionById)
 router.delete("/:id", deleteTransaction);
-
+router.get("/stats",getMonthStats)
 
 export default router
