0886171c13bc619f848bbf1b3fec81e6fe1f86f0
ayoubhanine <ayoubhanine2000@gmail.com>
2026-03-04 09:52:36 +0000

    creation des controllers(pagination+filtres)+midllwares+routes

---
+++ controllers/POSTtransaction.js
@@ -0,0 +1,15 @@
+import Transaction from "../models/Transaction.js";
+
+export const addTransaction = async (req, res) => {
+  try {
+    const transaction = new Transaction(req.body);
+    const savedTransaction = await transaction.save();
+    res.status(201).json({
+      message: "Transaction ajoutée avec succès",
+      transaction: savedTransaction,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Erreur serveur lors de l'ajout de la transaction" });
+  }
+};
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import routesTransactions from "./routes/routesTransactions.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use("/", routesTransactions);
 const MONGODB_URL = process.env.MONGO_URI;
 
 try {
   await mongoose.connect(MONGODB_URL);
-  console.log("connected to MongoDB");
+  console.log("connected to MongoDBb");
 } catch (err) {
   console.log(err);
 }
