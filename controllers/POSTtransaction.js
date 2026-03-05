558eba1b74db24473cff079b9f4f3cc04ec5badd
ayoubhanine <ayoubhanine2000@gmail.com>
2026-03-05 10:32:28 +0000

    add some comments

--- controllers/POSTtransaction.js
+++ controllers/POSTtransaction.js
@@ -2,7 +2,7 @@ import Transaction from "../models/Transaction.js";
 
 export const addTransaction = async (req, res) => {
   try {
-    const transaction = new Transaction(req.body);
+    const transaction = new Transaction(req.body); //req.body contient les données envoyées depuis Postman ou le frontend
     const savedTransaction = await transaction.save();
     res.status(201).json({
       message: "Transaction ajoutée avec succès",
